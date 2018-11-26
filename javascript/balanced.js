0286d3bc5392fec4de74a9ce73068523a3eb1890
David Thorn <david.thorn221278@googlemail.com>
2018-11-26 04:36:28 +0100

    initial commit

---
+++ javascript/balanced.js
@@ -0,0 +1,23 @@
+
+function balanced(str) {
+
+    /// remove all white space first and then remove all occurences of ()
+    let result = str.replace(/[^\(\)]/g, "").replace(/\(\)/g , "")
+
+    /// if matches is an array that means that match found more
+    /// which means we can call balanced on matches again
+    /// if results length is more than 0 then we have a single bracket
+    let d = { matches : result.match(/\(\)+/g) , result }
+
+    /// if matches is null and result.length is 0 then its balanced else not
+    return d.matches !== null ? balanced(result) : d.result.length === 0 ? true : false
+}
+
+console.log(balanced("(() containing text ( () )")) // false
+console.log(balanced("(() ( () )")) // false
+console.log(balanced("(() containing text ( () ))")) // true
+
+
+console.log(balanced("(()()(()))")) // true
+console.log(balanced("())()(()")) // false
+console.log(balanced("((()())())((")) // false
