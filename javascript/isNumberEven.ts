f7ab24646e00ddef28088f61a4022b20b914162b
David Thorn <david.thorn221278@googlemail.com>
2018-11-28 09:50:39 +0100

    Update isNumberEven.ts

--- javascript/isNumberEven.ts
+++ javascript/isNumberEven.ts
@@ -1,14 +1,12 @@
-
 /**
  * Returns if the number is Event
+ * If prev is undefined then the num will be used 
  *
  * @param {number} num
+ * @param {number} [prev=undefined]
+ * @param {boolean} [state=true]
  * @returns {boolean}
  */
-function isNumberEven(num: number): boolean {
-    let isEven = true
-    /// The number is even in the beginning therefore 
-    /// everytime we iterate through the number we will negate isEven 
-    for (let x: number = 0; x < num; x++) { isEven = !isEven }
-    return isEven
+function isNumberEven(num: number, prev: number = undefined , state: boolean = true): boolean {
+    return prev === 0 ? state : isNumberEven(num , prev === undefined ? --num : --prev , !state)
 }
